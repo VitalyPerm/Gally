f58c944bad1a716efb71979acaf8f3f4f9a7adbd
vitaly <kuraev_vf@mail.ru>
2024-01-03 15:40:13 +0500

    in progress

--- core/src/main/java/ru/kvf/core/widgets/PhotosPager.kt
+++ core/src/main/java/ru/kvf/core/widgets/PhotosPager.kt
@@ -7,6 +7,8 @@ import androidx.compose.foundation.pager.HorizontalPager
 import androidx.compose.foundation.pager.PagerState
 import androidx.compose.material3.Card
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.setValue
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.graphics.graphicsLayer
@@ -24,6 +26,14 @@ fun PhotosPager(
     pagerState: PagerState,
     loading: Boolean
 ) {
+    /*
+      var zoom by remember { mutableStateOf(1f) }
+    val transformationState = rememberTransformableState { zoomChange, offsetChange, rotationChange ->
+        zoom = (zoom * zoomChange).takeIf { it > 1f } ?: 1f
+        log("zoomChange = $zoomChange zoom = $zoom")
+    }
+     */
+
     HorizontalPager(
         state = pagerState,
         modifier = modifier
