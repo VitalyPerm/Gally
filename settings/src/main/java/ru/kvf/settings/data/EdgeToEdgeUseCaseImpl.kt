029d9f7b8a9384f17fbc65f095aca572319119e3
vitaly <kuraev_vf@mail.ru>
2024-05-05 13:06:23 +0500

    inP

--- settings/src/main/java/ru/kvf/settings/data/EdgeToEdgeUseCaseImpl.kt
+++
@@ -1,4 +0,0 @@
-package ru.kvf.settings.data
-
-private const val EDGE_TO_EDGE_KEY = "edge_to_edge_key"
-
--- settings/src/main/java/ru/kvf/settings/data/ThemeUseCaseImpl.kt
+++
@@ -1,4 +0,0 @@
-package ru.kvf.settings.data
-
-private const val THEME_KEY = "theme_key"
-
